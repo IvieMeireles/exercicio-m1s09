eb0ee0aea28167a65a8fb3d50fb582bcda6fd100
IvieMeireles <iviemeireles@gmail.com>
2022-12-04 15:05:19 -0300

    Card alterado para conter os dados dentro do .ts

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent {
-
+personagem: any = {
+  nomePersonagem: 'Jon Snow',
+  imagem: 'https://tm.ibxk.com.br/2022/08/26/26174558850404.jpg? ims=704x264',
+  nomeAtor: 'Christopher Catesby Harington',
+  descricao: 'Christopher Catesby Harington é um ator britânico. É mais conhecido por interpretar Jon Snow na série televisiva Game of Thrones da HBO.'
+}
 }
